a28252674ada85856e8e9d5844713b65584ce4c3
Vlad <akhtyamov.vlad@outlook.com>
2018-06-13 23:40:17 +0300

    Добавлена начальная имплементация для бд

--- lib/kvstore/storage.ex
+++ lib/kvstore/storage.ex
@@ -1,19 +1,32 @@
 #TODO: Этот модуль должен реализовать механизмы CRUD для хранения данных. Если одного модуля будет мало, то допускается создание модулей с префиксом "Storage" в названии
 defmodule Kvstore.Storage do
+  alias Kvstore.Utils
 
   def start() do
+    :dets.open_file(:storage, [type: :set])
   end
 
-  def create(data, ttl) do
+  def create(data, _ttl) do
+    if :dets.insert_new(:storage, data) do
+      {:ok}
+    else
+      {:error, "This key-field is already exist in storage!"}
+    end
   end
 
-  def read(fields) do
+  def read(keys) do
+    keys = Utils.parse(:keys, keys)
+    :dets.match_object(:storage, keys)
   end
 
   def update(old_data, new_data) do
+    :dets.delete(old_data)
+    :dets.insert_new(new_data)
+    {:ok}
   end
 
   def delete(data) do
+    :dets.delete(:storage, data)
   end
 
 end
--- lib/kvstore/utils.ex
+++ lib/kvstore/utils.ex
@@ -1,3 +1,9 @@
 defmodule Kvstore.Utils do
 
+  def parse(:keys, keys) do
+    Application.get_env(:kvstore, :keys)
+    |> Map.new(fn {key, value} -> {key, :"_"} end)
+    |> Map.merge(keys)
+  end
+
 end
