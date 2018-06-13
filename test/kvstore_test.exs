2eac08040346553da08c2783f8659a8719a0ac71
Vlad <akhtyamov.vlad@outlook.com>
2018-06-13 16:40:24 +0300

    Добавлены тесты, имплементирующие CRUD и временность

--- test/kvstore_test.exs
+++ test/kvstore_test.exs
@@ -1,1 +1,30 @@
-Тестируем как можно больше кейсов.
+defmodule KvstoreTests do
+  use ExUnit.Case
+
+  @result_for_read {:ok, %{id: 1, username: "Vlad", rules: :admin, date: DateTime.utc_now()}}
+
+  test "creating new row" do
+    assert Kvstore.Storage.create(%{id: 1, username: "Vlad", rules: :admin, date: DateTime.utc_now()}, %{ttl: :infinit}) == {:ok}
+  end
+
+  test "read row on field :id" do
+    assert Kvstore.Storage.read(%{id: 1}) == @result_for_read
+  end
+
+  test "read row on field :username" do
+    assert Kvstore.Storage.read(%{username: "Vlad"}) == @result_for_read
+  end
+
+  test "read row on field :rules" do
+    assert Kvstore.Storage.read(%{rules: :admin}) == @result_for_read
+  end
+
+  test "update row" do
+    assert Kvstore.Storage.update(%{id: 1}, %{username: "Akhtyamov"}) == {:ok, %{id: 1, username: "Akhtyamov", rules: :admin}}
+  end
+
+  test "temple?" do
+    Kvstore.Storage.create(%{id: 2, date: DateTime.utc_now()}, %{ ttl: 1})
+    refute Kvstore.Storage.read(%{id: 2}) == @result_for_read
+  end
+end
