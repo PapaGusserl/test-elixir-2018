6fd6e011c0a7ab6e655c1c15baf69640ec9a9c15
Vlad <akhtyamov.vlad@outlook.com>
2018-06-14 09:57:38 +0300

    Элементарная имплементация веб-сервера

--- lib/kvstore/router.ex
+++ lib/kvstore/router.ex
@@ -1,11 +1,44 @@
 #TODO: Для веб сервера нужен маршрутизатор, место ему именно тут.
 defmodule Kvstore.Router do
   use Plug.Router
+  alias Kvstore.Storage
 
-  plug(:match)
-  plug(:dispatch)
+  plug :match
+  plug :dispatch
+  plug Plug.Logger, log: :debug
 
-  get("/", do: send_resp(conn, 200, "Connect"))
-  match(_, do: send_resp(conn, 404, "Ooops.."))
+  get "/", do: send_resp(conn, 200, "Connect")
+
+  post "/create" do
+    body = valid? :data, 
+                  conn.body_params, 
+                  Storage.create(conn.body_params, conn.body_params.ttl)
+    send_resp(conn, 200, body)    
+  end
+
+  post "/read" do
+    body = valid? :keys, 
+                  conn.body_params,
+                  Storage.read(conn.body_params)
+    send_resp(conn, 200, body)    
+  end
+
+
+  post "/update" do
+    body = valid? :data, 
+                  conn.body_params.old_data, 
+                  conn.body_params.new_data, 
+                  Storage.update(conn.body_params.old_data, conn.body_params.new_data)
+    send_resp(conn, 200, body)
+  end
+
+  post "/delete" do
+    body = valid? :data,
+                  conn.body_params,
+                  Storage.delete(conn.body_params)
+    send_resp(conn, 200, body)
+  end
+
+  match _, do: send_resp(conn, 404, "Ooops..")
 
 end
--- lib/kvstore/utils.ex
+++ lib/kvstore/utils.ex
@@ -1,3 +1,15 @@
 defmodule Kvstore.Utils do
 
+  def valid?(:data, data, fun) do
+  end
+
+  def valid?(:data, old_data, new_data, fun) do
+  end
+
+  def valid?(:keys, keys) do
+  end
+
+  def valid?(:values, data) do
+  end
+
 end
